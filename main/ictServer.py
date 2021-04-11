5dd1275af9903cc79aeca65757a5eef6081a6994
dietpete <Peter.dietzler@sensitec.com>
2021-04-11 12:56:23 +0200

    add funktion to get time from Internet and calculate Summer and Winter Time

--- main/ictServer.py
+++ main/ictServer.py
@@ -12,7 +12,6 @@ import ntptime
 import utime
 
 
-
 def check_for_ota_update(config_data):
     print('Starte ota updater check:')
     ota = OTAUpdater( config_data['wifi']['gitpath'] )
@@ -21,7 +20,6 @@ def check_for_ota_update(config_data):
 
 def initTime():
     print('initTime()....')
-
     rtc = RTC()
     rtc.datetime((2017, 8, 23, 1, 12, 48, 0, 0)) # set a specific date and time
     print('Time   : ', rtc.datetime()) # get date and time
@@ -34,9 +32,148 @@ def initTime():
     print('Time:', utime.gmtime())
     #print('Time:', utime.localtime())
     
+def get_ntp_time():
     
-    utime
     
+    print('get_ntp_time().... ')
+    
+    year = utime.localtime()[0]       #get current year
+    now=ntptime.time()
+    print('getntptime() :', utime.localtime())
+    HHMarch   = utime.mktime((year,3 ,(31-(int(5*year/4+4))%7),1,0,0,0,0,0)) #Time of March change to CEST
+    HHOctober = utime.mktime((year,10,(31-(int(5*year/4+1))%7),1,0,0,0,0,0)) #Time of October change to CET
+    
+    print('HHMarch :', HHMarch)
+    print('HHOctober :', HHOctober)
+    print('ntptime.NTP_DELTA :', ntptime.NTP_DELTA)
+    
+    if now < HHMarch :               # we are before last sunday of march
+        ntptime.NTP_DELTA = 3155673600-3600 # CET:  UTC+1H
+        print('ntptime.NTP_DELTA before last sunday of march:', ntptime.NTP_DELTA)
+    elif now < HHOctober :           # we are before last sunday of october
+        ntptime.NTP_DELTA = 3155673600-7200 # CEST: UTC+2H
+        print('ntptime.NTP_DELTA before last sunday of october:', ntptime.NTP_DELTA)
+    else:                            # we are after last sunday of october
+        ntptime.NTP_DELTA = 3155673600-3600 # CET:  UTC+1H
+        print('ntptime.NTP_DELTA : after last sunday of october', ntptime.NTP_DELTA)
+    
+    ntptime.settime() # set the rtc datetime from the remote server
+
+
+def getntptime():
+    now = 0
+    ntptime.host = 'nz.pool.ntp.org'
+    count = 0
+
+    print('getntptime()... ')
+
+    while (count < 10):
+        count += 1
+
+        try:
+            now = ntptime.time()
+        except OSError as error:
+            machine.reset()
+
+        print('.', end = '')
+        utime.sleep(1)
+
+        if (now > 660000000):
+            count = 0
+            break
+
+    if (count == 10):
+        count = 0
+        machine.reset()
+
+    print(' got epoch')
+    utime.sleep(1)
+
+
+ #  only correct for 2021 New Zealand
+    HHApril = utime.mktime((2021,4,4,3,0,0,0,0,0)) #  time of April change to NZST
+    HHSept = utime.mktime((2021,9,26,2,0,0,0,0,0)) #  time of Sept change to NZDT
+
+    if now < HHApril:               # we are before the first Sunday of April
+        ntptime.NTP_DELTA = 3155673600 - (13 * 3600) #  NZDT: UTC-13H
+    elif now < HHSept:              # we are before the last Sunday of Sept
+        ntptime.NTP_DELTA = 3155673600 - (12 * 3600) #  NZST: UTC-12H
+    else:                           # we are after the last Sunday of Sept
+        ntptime.NTP_DELTA = 3155673600 - (13 * 3600) #  NZDT: UTC-13H
+
+ #  set the time
+    count = 0
+
+    while (count < 10):
+        count += 1
+
+        my_time = ntptime.settime() # set the rtc datetime from the remote server
+
+        print('.', end = '')
+        utime.sleep(1)
+
+        if (str(my_time) == 'None'):
+            count = 0
+            break
+
+    if (count == 10):
+        count = 0
+        machine.reset()
+
+    print(' time has been set')
+    utime.sleep(1)
+    
+  
+def resolve_dst_and_set_time():
+    global TIMEZONE_DIFFERENCE
+    global dst_on
+    dst_on = 1
+    TIMEZONE_DIFFERENCE = 1
+    print(' resolve_dst_and_set_time()..... ')
+    
+    # This is most stupid thing what humans can do!
+    # Rules for Finland: DST ON: March last Sunday at 03:00 + 1h, DST OFF: October last Sunday at 04:00 - 1h
+    # Sets localtime to DST localtime
+    '''
+    if network.WLAN(network.STA_IF).config('essid') == '':
+        now = mktime(localtime())
+        if DEBUG_ENABLED == 1:
+            print("Network down! Can not set time from NTP!")
+    else:
+        now = ntptime.time()
+    '''
+    now = ntptime.time()
+    #(year, month, mdate, hour, minute, second, wday, yday) = utime.localtime(now)
+    
+    year = utime.localtime(now)[0]  
+    if year < 2020:
+        if DEBUG_ENABLED == 1:
+            print("Time not set correctly!")
+        return False
+
+    dstend = utime.mktime((year, 10, (31 - (int(5 * year / 4 + 1)) % 7), 4, 0, 0, 0, 6, 0))
+    dstbegin = utime.mktime((year, 3, (31 - (int(5 * year / 4 + 4)) % 7), 3, 0, 0, 0, 6, 0))
+
+    if TIMEZONE_DIFFERENCE >= 0:
+        if (now > dstbegin) and (now < dstend):
+            dst_on = True
+            ntptime.NTP_DELTA = 3155673600 - ((TIMEZONE_DIFFERENCE + 1) * 3600)
+        else:
+            dst_on = False
+            ntptime.NTP_DELTA = 3155673600 - (TIMEZONE_DIFFERENCE * 3600)
+    else:
+        if (now > dstend) and (now < dstbegin):
+            dst_on = False
+            ntptime.NTP_DELTA = 3155673600 - (TIMEZONE_DIFFERENCE * 3600)
+        else:
+            dst_on = True
+            ntptime.NTP_DELTA = 3155673600 - ((TIMEZONE_DIFFERENCE + 1) * 3600)
+    if dst_on is None:
+        if DEBUG_ENABLED == 1:
+            print("DST calculation failed!")
+            return False
+    else:
+        ntptime.settime()  
  
 
 class ictServer:
@@ -94,6 +231,10 @@ class ictServer:
             
             check_for_ota_update(config_data)
             initTime()
+            getntptime()
+            get_ntp_time()
+            resolve_dst_and_set_time()
+            print('current Time:', utime.gmtime())
             pass
         else:
             print('ERROR setAP = ', config_data["wifi"]["setAP"])
