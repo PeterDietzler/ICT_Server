36d876cc7b2ab12fded69864c3564d717e37d8a6
dietpete <Peter.dietzler@sensitec.com>
2021-04-17 22:31:13 +0200

    Vercheichnis Struktur eingebaut

--- code/main.py
+++ code/main.py
@@ -1,12 +1,12 @@
 import ujson
 import utime
-from ota_update.ota_updater import OTAUpdater
-from main.ictServer import ictServer
+from lib.ota_update.ota_updater import OTAUpdater
+from bin.ictServer import ictServer
 
 def download_and_install_update_if_available(config_data):
     #print('==== download_and_install_update_if_available() ====')
     if 'wifi' in config_data:
-        ota = OTAUpdater( config_data['wifi']['gitpath'] )
+        ota = OTAUpdater( config_data['wifi']['gitpath'], main_dir='bin' )
         ota.install_update_if_available_after_boot( config_data['wifi']['ssid'],
                                                     config_data['wifi']['password'])
     else:
@@ -16,14 +16,14 @@ def download_and_install_update_if_available(config_data):
 def start(config_data):
     global s
     #utime.sleep_ms(10000)
-    from main.ictServer import ictServer
+    from bin.ictServer import ictServer
     print('==== 2. start() =====')
     s = ictServer(config_data)
 
 
 def boot_ict_server():
     print(' ==== 1. boot_ict_server()  ====')
-    f = open('config.json')
+    f = open('etc/UserProfile.json')
     config_data = ujson.load(f)
     download_and_install_update_if_available(config_data)
     start(config_data)
--- code/tests/StubUTime.py
+++
@@ -1,7 +0,0 @@
-import time
-
-
-class StubUTime:
-
-    def ticks_ms(self):
-        return int(round(time.time() * 1000))
